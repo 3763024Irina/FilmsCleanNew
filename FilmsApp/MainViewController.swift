d2aeab60db8f112bfb27d1f6f274bf88f0c1c666
IriSakovich <sakovich@gmx.es>
2025-04-24 20:09:26 +0200

    custom avec poster

--- FilmsApp/MainViewController.swift
+++ FilmsApp/MainViewController.swift
@@ -9,8 +9,8 @@ class MainViewController: UIViewController, UICollectionViewDelegate, UICollecti
     private let collectionView: UICollectionView = {
         let layout = UICollectionViewFlowLayout()
         layout.scrollDirection = .vertical
-        layout.minimumInteritemSpacing = 8// Отступ между ячейками по горизонтали
-        layout.minimumLineSpacing = 10 // Отступ между строками
+        layout.minimumInteritemSpacing = 8
+        layout.minimumLineSpacing = 10
         layout.sectionInset = UIEdgeInsets(top: 20, left: 10, bottom: 20, right: 10)
         let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
         collectionView.translatesAutoresizingMaskIntoConstraints = false
@@ -32,32 +32,34 @@ class MainViewController: UIViewController, UICollectionViewDelegate, UICollecti
         setupSearchBar()
         setupCollectionView()
     
-            testArray = [
-                TestModel(testPic: "image1", testTitle: "Inception", testYeah: "2010", testRating: "8.8"),
-                TestModel(testPic: "image2", testTitle: "Titanic", testYeah: "1997", testRating: "7.8"),
-                TestModel(testPic: "image3", testTitle: "Avatar", testYeah: "2009", testRating: "7.9"),
-                TestModel(testPic: "image4", testTitle: "The Dark Knight", testYeah: "2008", testRating: "9.0"),
-                TestModel(testPic: "image5", testTitle: "Forrest Gump", testYeah: "1994", testRating: "8.8"),
-                TestModel(testPic: "image6", testTitle: "The Matrix", testYeah: "1999", testRating: "8.7"),
-                TestModel(testPic: "image7", testTitle: "The Shawshank Redemption", testYeah: "1994", testRating: "9.3"),
-                TestModel(testPic: "image8", testTitle: "Gladiator", testYeah: "2000", testRating: "8.5"),
-                TestModel(testPic: "image9", testTitle: "The Godfather", testYeah: "1972", testRating: "9.2"),
-                TestModel(testPic: "image10", testTitle: "The Lion King", testYeah: "1994", testRating: "8.5"),
-                TestModel(testPic: "image11", testTitle: "Pulp Fiction", testYeah: "1994", testRating: "8.9"),
-                TestModel(testPic: "image12", testTitle: "Fight Club", testYeah: "1999", testRating: "8.8"),
-                TestModel(testPic: "image13", testTitle: "Interstellar", testYeah: "2014", testRating: "8.6")
-            ]
+        testArray = [
+         
+            TestModel(testPic: "image1", testTitle: "Inception", testYeah: "2010", testRating: "8.8"),
+            TestModel(testPic: "image2", testTitle: "Titanic", testYeah: "1997", testRating: "7.8"),
+            TestModel(testPic: "image3", testTitle: "Avatar", testYeah: "2009", testRating: "7.9"),
+            TestModel(testPic: "image4", testTitle: "The Dark Knight", testYeah: "2008", testRating: "9.0"),
+            TestModel(testPic: "image5", testTitle: "Forrest Gump", testYeah: "1994", testRating: "8.8"),
+            TestModel(testPic: "image6", testTitle: "The Matrix", testYeah: "1999", testRating: "8.7"),
+            TestModel(testPic: "image7", testTitle: "The Shawshank Redemption", testYeah: "1994", testRating: "9.3"),
+            TestModel(testPic: "image8", testTitle: "Gladiator", testYeah: "2000", testRating: "8.5"),
+            TestModel(testPic: "image9", testTitle: "The Godfather", testYeah: "1972", testRating: "9.2"),
+            TestModel(testPic: "image10", testTitle: "The Lion King", testYeah: "1994", testRating: "8.5"),
+            TestModel(testPic: "image11", testTitle: "Pulp Fiction", testYeah: "1994", testRating: "8.9"),
+            TestModel(testPic: "image12", testTitle: "Fight Club", testYeah: "1999", testRating: "8.8"),
+            TestModel(testPic: "image13", testTitle: "Interstellar", testYeah: "2014", testRating: "8.6"),
+            TestModel(testPic: "image14", testTitle: "Рабыня Изаура", testYeah: "1985", testRating: "9.0"),
+            TestModel(testPic: "image15", testTitle: "Добрыня Никитич", testYeah: "2000", testRating: "9.5")
+        ]
 
-        
         filteredArray = testArray
     }
+
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         navigationController?.navigationBar.isHidden = false
         title = "Films"
     }
 
-    
     private func setupSearchBar() {
         searchBar.delegate = self
         view.addSubview(searchBar)
@@ -68,16 +70,14 @@ class MainViewController: UIViewController, UICollectionViewDelegate, UICollecti
             searchBar.trailingAnchor.constraint(equalTo: view.trailingAnchor)
         ])
     }
-    
+
     private func setupCollectionView() {
         collectionView.delegate = self
         collectionView.dataSource = self
         
-        // Зарегистрировать вашу кастомную ячейку
         let nib = UINib(nibName: "MyCustomCell", bundle: nil)
         collectionView.register(nib, forCellWithReuseIdentifier: "MyCustomCell")
 
-        
         view.addSubview(collectionView)
         
         NSLayoutConstraint.activate([
@@ -87,45 +87,50 @@ class MainViewController: UIViewController, UICollectionViewDelegate, UICollecti
             collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
         ])
     }
-    
+
     // MARK: - UICollectionView DataSource
-    
+
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return isFiltering ? filteredArray.count : testArray.count
     }
-    
+
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MyCustomCell", for: indexPath) as! MyCustomCell
         let model = isFiltering ? filteredArray[indexPath.row] : testArray[indexPath.row]
-        
+
         if let filmTitle = model.testTitle, let releaseYear = model.testYeah, let rating = model.testRating, let posterPreview = model.testPic {
             let isImageOnly = posterPreview.isEmpty
-            
+
             let myModel = MyCustomCell.MyModel(filmTitle: filmTitle, releaseYeah: releaseYear, rating: rating, posterPreview: posterPreview, isImageOnly: isImageOnly)
             cell.configure(with: myModel)
+
+            cell.onDoubleTap = { [weak self] in
+                let fullPosterVC = PosterFullViewController()
+                fullPosterVC.posterImageName = posterPreview
+                fullPosterVC.modalPresentationStyle = .fullScreen
+                self?.present(fullPosterVC, animated: true, completion: nil)
+            }
         }
-        
+
         return cell
     }
-    
+
     // MARK: - UICollectionView DelegateFlowLayout
-    
-    // Размеры для ячеек
+
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         let layout = collectionViewLayout as! UICollectionViewFlowLayout
         let insets = layout.sectionInset
         let spacing = layout.minimumInteritemSpacing
-        
+
         let totalSpacing = insets.left + insets.right + spacing
         let itemWidth = (collectionView.frame.width - totalSpacing) / 2
-        let itemHeight: CGFloat = 250 // можешь менять под контент
+        let itemHeight: CGFloat = 250
 
         return CGSize(width: itemWidth, height: itemHeight)
     }
 
-    
     // MARK: - SearchBar Delegate
-    
+
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
         isFiltering = !searchText.isEmpty
         
@@ -136,15 +141,15 @@ class MainViewController: UIViewController, UICollectionViewDelegate, UICollecti
         } else {
             filteredArray = testArray
         }
-        
+
         collectionView.reloadData()
     }
+
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         let selectedFilm = isFiltering ? filteredArray[indexPath.row] : testArray[indexPath.row]
-        
+
         let detailVC = DetailFilmViewController()
         detailVC.film = selectedFilm
         navigationController?.pushViewController(detailVC, animated: true)
     }
-
 }
--- FilmsApp/MyCustomCell.swift
+++ FilmsApp/MyCustomCell.swift
@@ -1,11 +1,18 @@
 import UIKit
 
+protocol PosterTapDelegate: AnyObject {
+    func didDoubleTapPoster(image: UIImage)
+}
+
 class MyCustomCell: UICollectionViewCell {
     
     @IBOutlet weak var posterImageView: UIImageView!
     @IBOutlet weak var filmTitleLabel: UILabel!
     @IBOutlet weak var releaseYearLabel: UILabel!
     @IBOutlet weak var ratingLabel: UILabel!
+    
+    weak var delegate: PosterTapDelegate?
+
     struct MyModel {
         let filmTitle: String
         let releaseYeah: String
@@ -13,31 +20,64 @@ class MyCustomCell: UICollectionViewCell {
         let posterPreview: String
         let isImageOnly: Bool
     }
+    
+    var onDoubleTap: (() -> Void)?
 
     override func awakeFromNib() {
         super.awakeFromNib()
+        setupUI()
+        setupGesture()
+    }
 
+    private func setupUI() {
         filmTitleLabel.numberOfLines = 0
         filmTitleLabel.lineBreakMode = .byWordWrapping
-        filmTitleLabel.font = UIFont.systemFont(ofSize: 16)
+        filmTitleLabel.font = .systemFont(ofSize: 16, weight: .semibold)
+        filmTitleLabel.textColor = .black
 
-        releaseYearLabel.font = UIFont.systemFont(ofSize: 14)
-        ratingLabel.font = UIFont.systemFont(ofSize: 14)
+        releaseYearLabel.font = .systemFont(ofSize: 14)
+        releaseYearLabel.textColor = .darkGray
+
+        ratingLabel.font = .systemFont(ofSize: 14)
+        ratingLabel.textColor = .systemBlue
 
         posterImageView.contentMode = .scaleAspectFill
         posterImageView.clipsToBounds = true
+        posterImageView.layer.cornerRadius = 8
+        posterImageView.isUserInteractionEnabled = true
     }
 
+    private func setupGesture() {
+        let doubleTap = UITapGestureRecognizer(target: self, action: #selector(handleDoubleTap))
+        doubleTap.numberOfTapsRequired = 2
+        posterImageView.addGestureRecognizer(doubleTap)
+    }
 
-    required init?(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)
+    @objc private func handleDoubleTap() {
+        if let onDoubleTap = onDoubleTap {
+            onDoubleTap()
+        } else if let image = posterImageView.image {
+            delegate?.didDoubleTapPoster(image: image)
+        }
     }
 
     func configure(with model: MyModel) {
-        filmTitleLabel.text = model.filmTitle
-        releaseYearLabel.text = model.releaseYeah
-        ratingLabel.text = model.rating
-        posterImageView.image = UIImage(named: model.posterPreview)
-    }
+        let image = UIImage(named: model.posterPreview) ?? UIImage(named: "placeholder")
+        posterImageView.image = image
+        
+        let isTextHidden = model.isImageOnly
+        filmTitleLabel.isHidden = isTextHidden
+        releaseYearLabel.isHidden = isTextHidden
+        ratingLabel.isHidden = isTextHidden
 
+        if !isTextHidden {
+            filmTitleLabel.text = model.filmTitle
+            releaseYearLabel.text = model.releaseYeah
+            ratingLabel.text = "⭐️ \(model.rating)"
+        } else {
+            filmTitleLabel.text = nil
+            releaseYearLabel.text = nil
+            ratingLabel.text = nil
+        }
+    }
 }
